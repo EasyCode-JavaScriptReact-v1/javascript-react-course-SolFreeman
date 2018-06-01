f7ec7c2225fb4cfb20dc0c6bfd1d9e8ec60bac41
SolFreeman <soloviyov.stas@gmail.com>
2018-06-01 17:29:16 +0300

    homework_1

---
+++ js-core/homeworks/homework-1/src/main.js
@@ -0,0 +1,20 @@
+const contacts = {
+    name: 'Stas',
+    lastName: 'Freeman ',
+    address:{
+        street:'Academica Pavlova',
+        buldingNumber: '149',
+        flat: null
+    },
+    photo: NaN,
+    phones:[
+        '0958577270',
+        '0971324233'
+    ],
+    messengers:{
+        viber: '0958577270'
+    },
+    premiumAccount: false
+};
+
+console.log(contacts);
